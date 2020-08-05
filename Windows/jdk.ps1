37228d96ea446b36aec5f0206d71c08d8846cd88
Allan Taylor <allan@startmail.com>
2020-08-05 07:40:17 -0400

    Update jdk.ps1

--- Windows/jdk.ps1
+++ Windows/jdk.ps1
@@ -4,10 +4,7 @@ $client = new-object System.Net.WebClient
 $cookie = "oraclelicense=accept-securebackup-cookie"
 $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
 $client.downloadFile($source, $destination)
-cmd /c start /wait c:\jdk.exe /s
-cd "C:\Program Files\Java"
-cd jdk*
-setx /M PATH $($Env:JAVA_HOME + ';.') 
-cd bin
-setx /M PATH $($Env:PATH + ';.') 
+cmd /c start /wait c:\jdk.exe /s /INSTALLDIRPUBJRE=C:\java
+$env:Path += ";c:\java\bin"
+$env:JAVA_HOME = "c:\java"
 Remove-Item -Path C:\jdk.exe -Force
