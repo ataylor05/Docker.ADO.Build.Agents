ef9ec7b5eb8d7e260bf5e7b8dbf181d6b2050f36
Allan Taylor <allan@startmail.com>
2020-08-18 08:52:28 -0400

    Update start.sh

--- Linux/start.sh
+++ Linux/start.sh
@@ -5,8 +5,6 @@ if [ -n "$AZP_WORK" ]; then
   mkdir -p "$AZP_WORK"
 fi
 
-AZP_TOKEN_FILE=/azp/.token
-
 mkdir /azp/agent
 cd /azp/agent
 
@@ -18,29 +16,14 @@ export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,KUBERNETES_PORT,KUBERNETES_PORT_443_TC
 export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,AdoAgentUrl,DotNetCoreUrl,GoUrl,Helm3Url,NodeJsUrl,PackerUrl,SalesForceCliUrl,TerraformUrl,VaultUrl,DEBIAN_FRONTEND,HOME,HOSTNAME,OLDPWD,PATH,PWD,sh,_
 export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,VSO_AGENT_IGNORE
 
-
-AZP_AGENT_RESPONSE=$(curl -LsS \
-  -u user:$(cat "$AZP_TOKEN_FILE") \
-  -H 'Accept:application/json;api-version=3.0-preview' \
-  "$AZP_URL/_apis/distributedtask/packages/agent?platform=linux-x64")
-
-if echo "$AZP_AGENT_RESPONSE" | jq . >/dev/null 2>&1; then
-  AZP_AGENTPACKAGE_URL=$(echo "$AZP_AGENT_RESPONSE" \
-    | jq -r '.value | map([.version.major,.version.minor,.version.patch,.downloadUrl]) | sort | .[length-1] | .[3]')
-fi
-
-if [ -z "$AZP_AGENTPACKAGE_URL" -o "$AZP_AGENTPACKAGE_URL" == "null" ]; then
-  echo 1>&2 "error: could not determine a matching Azure Pipelines agent - check that account '$AZP_URL' is correct and the token is valid for that account"
-  exit 1
-fi
-
-curl -LsS $AZP_AGENTPACKAGE_URL | tar -xz & wait $!
+AZP_TOKEN=$(cat /azp/.token)
+rm -f /azp/.token
 
 ./config.sh --unattended \
   --agent "$(hostname)" \
   --url "$AZP_URL" \
   --auth PAT \
-  --token $(cat "$AZP_TOKEN_FILE") \
+  --token $AZP_TOKEN \
   --pool "$AZP_POOL" \
   --work "$AZP_WORK" \
   --replace \
@@ -52,4 +35,4 @@ cd bin
 cd ..
 ./config.sh remove --unattended \
       --auth PAT \
-      --token $(cat "$AZP_TOKEN_FILE")
+      --token $AZP_TOKEN
