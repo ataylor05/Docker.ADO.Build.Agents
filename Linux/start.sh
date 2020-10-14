da04d0ed7a77ac0584af2ca18b33fa3bf69aa19f
Allan Taylor <allan@startmail.com>
2020-10-14 07:51:24 -0400

    WIP

--- Linux/start.sh
+++ Linux/start.sh
@@ -7,14 +7,25 @@ fi
 
 export AGENT_ALLOW_RUNASROOT="1"
 
+AZP_TOKEN=$(cat /azp/.token)
+rm -f /azp/.token
+
 # Ignore environment variables
 export VSO_AGENT_IGNORE=AZP_TOKEN,AZP_TOKEN_FILE,AZP_POOL,AZP_URL,AZP_WORK,AGENT_ALLOW_RUNASROOT,Agent.HomeDirectory
 export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,KUBERNETES_PORT,KUBERNETES_PORT_443_TCP,KUBERNETES_PORT_443_TCP_ADDR,KUBERNETES_PORT_443_TCP_PORT,KUBERNETES_PORT_443_TCP_PROTO,KUBERNETES_SERVICE_HOST,KUBERNETES_SERVICE_PORT,KUBERNETES_SERVICE_PORT_HTTPS
 export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,AdoAgentUrl,DotNetCoreUrl,GoUrl,Helm3Url,NodeJsUrl,PackerUrl,SalesForceCliUrl,TerraformUrl,VaultUrl,DEBIAN_FRONTEND,HOME,HOSTNAME,OLDPWD,PATH,PWD,sh,_
 export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,VSO_AGENT_IGNORE
 
-AZP_TOKEN=$(cat /azp/.token)
-rm -f /azp/.token
+AZP_AGENT_RESPONSE=$(curl -LsS -u user:$(cat "$AZP_TOKEN_FILE") -H 'Accept:application/json;api-version=3.0-preview' "$AZP_URL/_apis/distributedtask/packages/agent?platform=linux-x64")
+if echo "$AZP_AGENT_RESPONSE" | jq . >/dev/null 2>&1; then
+  AZP_AGENTPACKAGE_URL=$(echo "$AZP_AGENT_RESPONSE" | jq -r '.value | map([.version.major,.version.minor,.version.patch,.downloadUrl]) | sort | .[length-1] | .[3]')
+fi
+if [ -z "$AZP_AGENTPACKAGE_URL" -o "$AZP_AGENTPACKAGE_URL" == "null" ]; then
+  echo 1>&2 "error: could not determine a matching Azure Pipelines agent - check that account '$AZP_URL' is correct and the token is valid for that account"
+  exit 1
+fi
+curl -LsS $AZP_AGENTPACKAGE_URL | tar -xz & wait $!
+
 
 ./config.sh --unattended \
   --agent "$(hostname)" \
